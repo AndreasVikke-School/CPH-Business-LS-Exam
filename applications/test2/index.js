4557ea514e29b376d911aeeeb7ba01b7437de73e
andreasvikke <azv@efio.dk>
2021-11-03 19:51:36 +0100

    New Image2

--- applications/test2/index.js
+++ applications/test2/index.js
@@ -1,7 +1,20 @@
 var http = require('http');
 
+const options = {
+  hostname: 'test1',
+  port: 80,
+  path: '/',
+  method: 'GET'
+}
+
 //create a server object:
 http.createServer(function (req, res) {
-    res.write('Hello World! from test2 image!'); //write a response to the client
+    const req = http.request(options, ress => {
+        console.log(`statusCode: ${ress.statusCode}`)
+
+        ress.on('data', d => {
+            res.write(d);
+        })
+    })
     res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
