62e9735dff8b20fd67b5112c7a41a2331c9fca35
andreasvikke <azv@efio.dk>
2021-11-04 16:25:57 +0100

    New Func for single photo

--- applications/api/src/main.go
+++ applications/api/src/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	pb "api/rpc"
@@ -11,6 +12,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 // getAlbums responds with the list of all albums as JSON.
@@ -29,9 +31,28 @@ func getPhotos(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, photos)
 }
 
+func getPhotoById(c *gin.Context) {
+	conn, err := grpc.Dial("service-photos.test")
+	if err != nil {
+		fmt.Println("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewPhotoProtoClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
+	photo, err := client.GetPhotoById(ctx, wrapperspb.Int32(int32(id)))
+
+	c.IndentedJSON(http.StatusOK, photo)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/photos", getPhotos)
+	router.GET("/photos/:id", getPhotos)
 
 	router.Run("0.0.0.0:8080")
 }
