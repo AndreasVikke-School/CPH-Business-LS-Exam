931ebd56e98b5f776daf2ddcbae60b24a8f26571
andreasvikke <azv@efio.dk>
2021-11-04 16:12:54 +0100

    Grpc Go Client Test

--- applications/api/src/main.go
+++ applications/api/src/main.go
@@ -1,28 +1,31 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"time"
+
+	pb "api/rpc"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang/protobuf/ptypes/empty"
+	"google.golang.org/grpc"
 )
-
-// album represents data about a record album.
-type photo struct {
-	AlbumId int    `json:"albumId"`
-	Id      int    `json:"id"`
-	Title   string `json:"Title"`
-	Url     string `json:"url"`
-}
-
-// albums slice to seed record album data.
-var photos = []photo{
-	{AlbumId: 1, Id: 1, Title: "accusamus beatae ad facilis cum similique qui sunt", Url: "https://via.placeholder.com/600/92c952"},
-	{AlbumId: 1, Id: 2, Title: "reprehenderit est deserunt velit ipsam", Url: "https://via.placeholder.com/600/771796"},
-	{AlbumId: 1, Id: 3, Title: "officia porro iure quia iusto qui ipsa ut modi", Url: "https://via.placeholder.com/600/24f355"},
-}
 
 // getAlbums responds with the list of all albums as JSON.
 func getPhotos(c *gin.Context) {
+	conn, err := grpc.Dial("test1.test")
+	if err != nil {
+		fmt.Println("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewPhotoProtoClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	photos, err := client.GetAllPhotos(ctx, &empty.Empty{})
+
 	c.IndentedJSON(http.StatusOK, photos)
 }
 
