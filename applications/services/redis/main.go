6fa9a5bd1005a9cb2408a89dc0b9060bb573bc32
andreasvikke <azv@efio.dk>
2021-11-28 12:26:12 +0100

    New Error

--- applications/services/redis/main.go
+++ applications/services/redis/main.go
@@ -26,13 +26,19 @@ type server struct {
 
 func (s *server) CreateAttendanceCode(ctx context.Context, in *pb.AttendanceCodeCreate) (*pb.AttendanceCode, error) {
 	code, unix, err := CreateAttendanceCodeInRedis(in.GetMinutesToLive(), configuration)
-	eh.PanicOnError(err, "Error when adding code to redis")
+	// eh.PanicOnError(err, "Error when adding code to redis")
+	if err != nil {
+		return nil, err
+	}
 	return &pb.AttendanceCode{Code: code, Unix: unix}, nil
 }
 
 func (s *server) GetAttendanceCode(ctx context.Context, in *wrapperspb.Int64Value) (*pb.AttendanceCode, error) {
 	code, unix, err := GetAttendanceCodeFromRedis(in.Value, configuration)
-	eh.PanicOnError(err, "Error when getting code from redis")
+	// eh.PanicOnError(err, "Error when getting code from redis")
+	if err != nil {
+		return nil, err
+	}
 	return &pb.AttendanceCode{Code: code, Unix: unix}, nil
 }
 
--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -1,2 +1,2 @@
-api_service_image    = "sha256-009609718257"
-redis_service_image  = "sha256-050891d7f385"
+api_service_image    = "sha256-94aaa67ef3c4"
+redis_service_image  = "sha256-0eb1b7f423f7"
