1a4cfcc4ca55e91fb656db81c46d2bbdda79f9a1
andreasvikke <azv@efio.dk>
2021-12-03 17:36:21 +0100

    Added Checkin Endpoints to API

--- applications/services/api/checkin.go
+++ applications/services/api/checkin.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"strconv"
 	"time"
 
 	eh "github.com/andreasvikke/CPH-Bussines-LS-Exam/applications/services/api/errorhandler"
+	pb "github.com/andreasvikke/CPH-Bussines-LS-Exam/applications/services/api/rpc"
+
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 type CheckIn struct {
@@ -14,6 +21,13 @@ type CheckIn struct {
 	CurrentUnixTime int64   `json:"currentUnixTime"`
 	Latitude        float64 `json:"lat"`
 	Longitude       float64 `json:"long"`
+}
+
+func GetCheckInServiceClient() pb.CheckInServiceClient {
+	conn, err := grpc.Dial(configuration.Postgres.Service, grpc.WithInsecure())
+	eh.PanicOnError(err, "fail to dial")
+
+	return pb.NewCheckInServiceClient(conn)
 }
 
 func ProduceCheckIn(c *gin.Context) {
@@ -26,4 +40,66 @@ func ProduceCheckIn(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{
 		"queued": "success",
 	})
+}
+
+func GetCheckInById(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	eh.PanicOnError(err, "Id is not an int")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	checkIn, err := GetCheckInServiceClient().GetCheckInById(ctx, &wrapperspb.Int64Value{Value: id})
+	eh.PanicOnError(err, "Failed to get checkin")
+
+	c.IndentedJSON(http.StatusOK, checkIn)
+}
+
+func GetCheckInsByStudentId(c *gin.Context) {
+	studentId := c.Param("studentId")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	checkIn, err := GetCheckInServiceClient().GetCheckInsByStudentId(ctx, &wrapperspb.StringValue{Value: studentId})
+	eh.PanicOnError(err, "Failed to get checkins")
+
+	c.IndentedJSON(http.StatusOK, checkIn)
+}
+
+func GetCheckInsByAttendenceCode(c *gin.Context) {
+	att := c.Param("attCode")
+	attCode, err := strconv.ParseInt(att, 10, 64)
+	eh.PanicOnError(err, "Attendance code is not an int")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	checkIn, err := GetCheckInServiceClient().GetCheckInsByAttendenceCode(ctx, &wrapperspb.Int64Value{Value: attCode})
+	eh.PanicOnError(err, "Failed to get checkins")
+
+	c.IndentedJSON(http.StatusOK, checkIn)
+}
+
+func GetCheckInsByTime(c *gin.Context) {
+	fromStr := c.Param("from")
+	toStr := c.Param("to")
+	from, err := strconv.ParseInt(fromStr, 10, 64)
+	eh.PanicOnError(err, "Attendance code is not an int")
+	to, err := strconv.ParseInt(toStr, 10, 64)
+	eh.PanicOnError(err, "Attendance code is not an int")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	checkIn, err := GetCheckInServiceClient().GetCheckInsByTime(ctx, &pb.TimeInterval{FromTime: from, ToTime: to})
+	eh.PanicOnError(err, "Failed to get checkins")
+
+	c.IndentedJSON(http.StatusOK, checkIn)
+}
+
+func GetAllCheckIns(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	checkIn, err := GetCheckInServiceClient().GetAllCheckIns(ctx, &emptypb.Empty{})
+	eh.PanicOnError(err, "Failed to get checkins")
+
+	c.IndentedJSON(http.StatusOK, checkIn)
 }
