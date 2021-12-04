47891eeea170ba7fca689b55daae6205253a9eac
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2021-12-05 00:49:29 +0100

    Fixed slow consumption of kafka messages

--- applications/services/consumer/Services/KafkaConsumer.cs
+++ applications/services/consumer/Services/KafkaConsumer.cs
@@ -6,8 +6,8 @@ namespace LSExam.Services;
 public class KafkaConsumer
 {
     private readonly ILogger _logger;
-    private readonly ConsumerBuilder<Null, string> _builder;
     private readonly string _topic;
+    private readonly IConsumer<Null, string> _consumer;
 
     public KafkaConsumer(ILogger<KafkaConsumer> logger, IOptions<KafkaSettings> options)
     {
@@ -21,23 +21,21 @@ public class KafkaConsumer
             AllowAutoCreateTopics = true,
             AutoOffsetReset = AutoOffsetReset.Earliest,
             SecurityProtocol = SecurityProtocol.Plaintext,
-            EnableAutoCommit = false
+            EnableAutoCommit = false,
         };
         _topic = option.CheckinTopic;
 
-        _builder = new ConsumerBuilder<Null, string>(config);
+        _consumer = new ConsumerBuilder<Null, string>(config).Build();
+        _consumer.Subscribe(_topic);
+        _consumer.Assign(new TopicPartition(_topic, Partition.Any));
     }
 
     public AttendanceEvent Poll(CancellationToken ct)
     {
-        using IConsumer<Null, string> consumer = _builder.Build();
-        consumer.Subscribe(_topic);
-        consumer.Assign(new TopicPartition(_topic, Partition.Any));
-
-        var result = consumer.Consume(ct);
+        var result = _consumer.Consume(ct);
         var message = result.Message;
 
-        consumer.Commit(result);
+        _consumer.Commit();
 
         AttendanceEvent value = JsonSerializer.Deserialize<AttendanceEvent>(message.Value) ?? new();
         _logger.LogInformation($"Consumed attandance event: {value}");
